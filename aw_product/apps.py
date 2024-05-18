6800c117651d743bd486475d86d270c6f6527d7c
Dianas <diana.rytko@mail.ru>
2024-05-18 15:45:16 +0300

    Initial commit

---
+++ aw_product/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AwProductConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aw_product'
+    verbose_name = 'Авто.бай'
